45161734d71e533345c24ffe26904ad7b798a7ce
Jorge Vergara <javebratt@gmail.com>
2016-11-05 15:37:01 -0500

    Creates the AuthData provider

--- src/providers/auth-data.ts
+++ src/providers/auth-data.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the AuthData provider.
+import { AngularFire } from 'angularfire2';
 
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class AuthData {
+  fireAuth: any;
+  constructor(public af: AngularFire) {
+    af.auth.subscribe( user => {
+      if (user) { this.fireAuth = user.auth; }
+    });
+  }
 
-  constructor(public http: Http) {
-    console.log('Hello AuthData Provider');
+  loginUser(newEmail: string, newPassword: string): any {
+    return this.af.auth.login({ email: newEmail, password: newPassword });
+  }
+
+  resetPassword(email: string): any {
+    return firebase.auth().sendPasswordResetEmail(email);
+  }
+
+  logoutUser(): any {
+    return this.af.auth.logout();
+  }
+
+  signupUser(newEmail: string, newPassword: string): any {
+    return this.af.auth.createUser({ email: newEmail, password: newPassword });
   }
 
 }
